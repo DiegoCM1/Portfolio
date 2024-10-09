e8d5a77ce9c63fa921c856fb15da0804fcbf4e34
Diego Colín <luiscolin764@gmail.com>
2024-10-08 23:38:03 -0600

    Added react to the project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Hello, this is React inside your existing project!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
