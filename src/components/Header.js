c7cac8a4a4672fe055e9430d9ba6d7d1adb185c9
Diego Colín <luiscolin764@gmail.com>
2025-02-20 11:27:37 -0600

    Changed color of main text in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
         {/* H1 - Title */}
         <TextGenerateEffect
           words="Frontend Developer"
-          className="text-3xl sm:text-5xl md:text-6xl font-bold text-highlightBlue dark:text-dark-accent"
+          className="text-3xl sm:text-5xl md:text-6xl font-bold text-buttonBlue dark:text-dark-accent"
           duration={300}  // Duration for animation
           delay={900}     // Delay of 0.5 seconds before starting
         />
