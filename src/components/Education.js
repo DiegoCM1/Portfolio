8636edfa8a3586ef25c27772120a6c253cbc8e82
Diego Colín <luiscolin764@gmail.com>
2025-05-08 14:46:24 -0600

    Added education details

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,22 +9,23 @@ const Education = () => {
       {/* Education List */}
       <ul className="text-base md:text-lg space-y-3 md:space-y-4">
         <li>
-          <a href="https://platzi.com/home/"> ● Platzi</a>
-        </li>
-        <li>
-          <a href="https://www.coderhouse.com/mx/?pipe_source=google&pipe_medium=cpc&pipe_campaign=0&gad_source=1&gad_campaignid=18826029104&gbraid=0AAAAACn2v4w9lSQvZqkCathLM_po4tl1C&gclid=Cj0KCQjwrPHABhCIARIsAFW2XBNxHcTXS95yipUSBYp-_DBcfhKKeUcsvaguygpyPRJ5CPyauI6BkM4aAtQiEALw_wcB">
-            {" "}
-            ● Coderhouse
+          <a href="https://platzi.com/home/" rel="noopener noreferrer" target="_blank"> ● Platzi – Web Development & AI Schools
           </a>
         </li>
         <li>
-          <a href="https://www.udemy.com/mobile/ipad/"> ● Udemy</a>
+          <a href="https://www.coderhouse.com/mx/?pipe_source=google&pipe_medium=cpc&pipe_campaign=0&gad_source=1&gad_campaignid=18826029104&gbraid=0AAAAACn2v4w9lSQvZqkCathLM_po4tl1C&gclid=Cj0KCQjwrPHABhCIARIsAFW2XBNxHcTXS95yipUSBYp-_DBcfhKKeUcsvaguygpyPRJ5CPyauI6BkM4aAtQiEALw_wcB" rel="noopener noreferrer" target="_blank">
+            {" "}
+            ● Coderhouse - Web Development Career
+          </a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/" rel="noopener noreferrer" target="_blank"> ● Udemy - AI and backend courses</a>
         </li>
 
         <li>
-          <a href="https://cloudonair.withgoogle.com/events/startup-school-ai-q2-2025?utm_source=cloud_sfdc&utm_medium=GFS%3DFY25-Q1-LATAM-SMB33145-onlineevent-er-Q2StartupSchool-27574&utm_content=GFSLP">
+          <a href="https://cloudonair.withgoogle.com/events/startup-school-ai-q2-2025?utm_source=cloud_sfdc&utm_medium=GFS%3DFY25-Q1-LATAM-SMB33145-onlineevent-er-Q2StartupSchool-27574&utm_content=GFSLP" rel="noopener noreferrer" target="_blank">
             {" "}
-            ● Google AI Startup School
+            ● Google AI Startup School - AI for Founders (2025)
           </a>
         </li>
       </ul>
