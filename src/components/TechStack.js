8751d39b7ad7048171221f21b8b74bd8b2a6fedd
Diego Colín <luiscolin764@gmail.com>
2025-05-22 19:14:34 -0600

    Added Next.js to tech stack

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -9,14 +9,15 @@ import {
   FaPython,
 } from "react-icons/fa";
 import { FaFigma } from "react-icons/fa6";
-import { SiTailwindcss, SiReact, SiVercel } from "react-icons/si";
+import { SiTailwindcss, SiReact, SiVercel, SiNextdotjs } from "react-icons/si";
 
 const TechStack = () => {
   return (
     <section className="pt-2">
-        <h3 className="text-2xl md:text-3xl font-bold mb-4 text-textWhite dark:text-dark-textPrimary pb-2">
-          Tech Stack<span className="text-buttonBlue dark:text-dark-accent">.</span>
-        </h3>
+      <h3 className="text-2xl md:text-3xl font-bold mb-4 text-textWhite dark:text-dark-textPrimary pb-2">
+        Tech Stack
+        <span className="text-buttonBlue dark:text-dark-accent">.</span>
+      </h3>
       <div className="flex flex-wrap md:justify-start gap-4 text-3xl md:text-4xl">
         {/* Individual Tech Item */}
         <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
@@ -34,6 +35,10 @@ const TechStack = () => {
         <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
           <FaReact className="text-blue-600 text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">React</p>
+        </div>
+        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+          <SiNextdotjs className="text-white text-4xl md:text-5xl" />
+          <p className="text-xs md:text-sm mt-2 text-textWhite">Next.js</p>
         </div>
         <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
           <SiTailwindcss className="text-teal-500 text-4xl md:text-5xl" />
@@ -56,7 +61,7 @@ const TechStack = () => {
           <p className="text-xs md:text-sm mt-2 text-textWhite">Python</p>
         </div>
         <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
-          <SiVercel className="text-black dark:text-white text-4xl md:text-5xl" />
+          <SiVercel className="text-white text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Vercel</p>
         </div>
       </div>
