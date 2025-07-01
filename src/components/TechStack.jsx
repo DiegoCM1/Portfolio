99daaf936dda2e89323a31f5df30cf15c22cff55
Diego Colín <luiscolin764@gmail.com>
2025-06-30 22:52:23 -0600

    Improved spacing on techstack icons

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -14,49 +14,49 @@ const TechStack = () => {
         Tech Stack
         <span className="text-buttonBlue dark:text-dark-accent">.</span>
       </h3>
-      <div className="flex flex-wrap md:justify-start gap-4 text-3xl md:text-4xl">
+      <div className="flex flex-wrap md:justify-start md:gap-4 text-3xl md:text-4xl">
         {/* Individual Tech Item */}
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <FaReact className="text-blue-600 text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">React</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <SiTailwindcss className="text-teal-500 text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Tailwind CSS</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <SiNextdotjs className="text-white text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Next.js</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <FaGit className="text-red-600 text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Git</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <FaGithub className="text-white text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">GitHub</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <SiVercel className="text-white text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Vercel</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <FaPython className="text-[#1b5e94] text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Python</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <SiFastapi className="text-green-300 text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">FastAPI</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center">
           <SiRailway className="text-white text-4xl md:text-5xl" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">Railway</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center ">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center ">
           <OpenRouterIcon className="text-white w-10 h-10 md:w-12 md:h-12" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">OpenRouter</p>
         </div>
-        <div className="flex flex-col items-center m-2 w-20 h-20 md:w-24 md:h-24 justify-center ">
+        <div className="flex flex-col items-center m-1 w-20 h-20 md:w-24 md:h-24 justify-center ">
           <SiOllama className="text-white w-10 h-10 md:w-12 md:h-12" />
           <p className="text-xs md:text-sm mt-2 text-textWhite">LLaMA Family</p>
         </div>
